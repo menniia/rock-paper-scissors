bfbcc484eaf58b466fef8143020c049f80fedba5
menniia <mennia17@gmail.com>
2024-09-08 23:40:10 +0000

    updated game files

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import CHOICES from "../utils/Choices";
 
-
 function Game() {
     const [playerChoice, setPlayerChoice] = useState(null);
     const [computerChoice, setComputerChoice] = useState(null);
@@ -25,6 +24,36 @@ function Game() {
         setComputerChoice(null);
         setResult(null);
     }
+
+    return (
+        <div>
+            <h1>Rock Paper Scissors</h1>
+            <div>
+                {CHOICES.map((choice) => (
+                    <button
+                        key={choice.name}
+                        onClick={handlePlayerChoice}
+                        aria-label={choice.name}>
+                        {choice.emoji}
+                    </button>
+                ))}
+            </div>
+            {playerChoice && computerChoice && (
+                <div>
+                    <div>
+                        <span>{playerChoice.emoji}</span>
+                        <p>You chose {playerChoice.name} </p>
+                    </div>
+                    <div>
+                        <span>{computerChoice.emoji}</span>
+                        <p>The computer chose {computerChoice.name} </p>
+                    </div>
+                    <h2>{result}</h2>
+                    <button onClick={resetGame}>Play Again</button>
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default Game;
